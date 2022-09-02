0575a098f5e8373b47ce5b749724a6f98116f8bd
KristianColville1 <kristiancolville1@gmail.com>
2022-09-02 14:51:18 +0100

    Added ham buger menu toggle for animation

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(() => {
+
+  // window scroll
   $(window).scroll(() => {
     var scrollHeight = 50;
     if($(window).scrollTop() >= scrollHeight){
@@ -6,5 +8,11 @@ $(document).ready(() => {
     } else if($(window).scrollTop() <= scrollHeight){
       $('nav.navbar').removeClass('nav-shadow-on').addClass('nav-shadow-off');
     }
+  });
+
+  // hamburger menu
+  $('button.navbar-toggler').on('click', ()=> {
+    let bars = document.querySelectorAll('.bar');
+    bars.forEach(bar => bar.classList.toggle('x'));
   });
 });
